45e3a74bf039e80abb9904ce71e3e67498d6a858
stormborax <stormborax@gmail.com>
2021-10-13 11:49:21 +0700

    modified output program of project 2

--- Session5/Project2/PesawatTempur.cs
+++ Session5/Project2/PesawatTempur.cs
@@ -4,6 +4,23 @@ namespace Project2
 {
     class PesawatTempur : Pesawat
     {
+        public PesawatTempur()
+        {
+            inputPesawatData();
+        }
+
+        private void inputPesawatData()
+        {
+            Console.Write("\nName: ");
+            this.name = Console.ReadLine();
+            Console.Write("Amount wheels: ");
+            this.wheelAmount = int.Parse(Console.ReadLine());
+            Console.Write("Passenger capacity: ");
+            this.passengerAmount = int.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            this.height = Console.ReadLine();
+        }
+
         public override void terbang()
         {
             Console.WriteLine("Pesawat tempur dengan nama: {0}, yang mempunyai jumlah roda: {1}, sedang berada pada ketinggian: {2} dengan membawa jumlah penumpang sebanyak {3} akan meledakkan senjata", this._name, this._wheelAmount, this._height, this._passengerAmount);
