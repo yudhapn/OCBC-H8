29588d05d70252dc6125997e9107f31dbd19a3f8
stormborax <stormborax@gmail.com>
2021-10-15 01:25:55 +0700

    added .gitignore

---
+++ Assignment2/tb_employee_202110140106.sql
@@ -0,0 +1,11 @@
+INSERT INTO db_bank.dbo.tb_employee (employeeNumber,lastName,firstName,extension,email,officeCode,reportsTo,jobTitle) VALUES
+	 (1,N'Leheude',N'Alvera',N'948-515-3702',N'aleheude0@skyrock.com',1,1,N'General manager'),
+	 (2,N'Lorenc',N'Wandie',N'948-432-3703',N'wlorenc1@devhub.com',1,1,N'Asset manager'),
+	 (3,N'de Juares',N'Kaycee',N'948-645-3704',N'kdejuares2@disqus.com',1,1,N'Relationship manager'),
+	 (4,N'MacNeilage',N'Niki',N'948-765-3705',N'nmacneilage3@nbcnews.com',2,3,N'Asset manager'),
+	 (5,N'Obert',N'Debby',N'948-324-3706',N'dobert4@blog.com',1,3,N'Bank teller'),
+	 (6,N'Heelis',N'Zollie',N'948-245-3707',N'zheelis5@ibm.com',1,2,N'Loan officer'),
+	 (7,N'Jefferson',N'Cassandre',N'543-518-3705',N'cjefferson6@umich.edu',2,3,N'Bank teller'),
+	 (8,N'Wyon',N'Chauncey',N'948-515-3805',N'cwyon7@intel.com',3,4,N' Financial analyst'),
+	 (9,N'Hofner',N'Zeke',N'948-515-9705',N'zhofner8@wsj.com',4,2,N' Financial analyst'),
+	 (10,N'Dagworthy',N'Libby',N'923-515-3705',N'ldagworthy9@jalbum.net',5,4,N'Financial advisor');
